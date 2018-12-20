3b316784e063a4999269028211f568903073cb61
jiangmin <xcodere@163.com>
2018-12-20 14:58:57 +0800

    '监控软件模板 给赖川做的'

---
+++ 监控软件模板/监控软件模板/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Orderbook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this._dgvOrderbook = new System.Windows.Forms.DataGridView();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._dgvOrderbook)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(139, 67);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(793, 361);
+            this.splitContainer1.SplitterDistance = 149;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _dgvOrderbook
+            // 
+            this._dgvOrderbook.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this._dgvOrderbook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dgvOrderbook.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Price,
+            this.Quantity});
+            this._dgvOrderbook.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._dgvOrderbook.Location = new System.Drawing.Point(2, 2);
+            this._dgvOrderbook.Margin = new System.Windows.Forms.Padding(2);
+            this._dgvOrderbook.Name = "_dgvOrderbook";
+            this._dgvOrderbook.RowTemplate.Height = 24;
+            this._dgvOrderbook.Size = new System.Drawing.Size(740, 274);
+            this._dgvOrderbook.TabIndex = 0;
+            // 
+            // Price
+            // 
+            this.Price.DataPropertyName = "Price";
+            this.Price.FillWeight = 71.06599F;
+            this.Price.HeaderText = "发布时间";
+            this.Price.Name = "Price";
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.FillWeight = 128.934F;
+            this.Quantity.HeaderText = "企业名称";
+            this.Quantity.Name = "Quantity";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this._dgvOrderbook, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.28532F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.71468F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(744, 361);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.statusStrip1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 281);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(738, 77);
+            this.panel1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 55);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(738, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(569, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "分钟/次";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(505, 20);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 21);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "起始日期：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "访问频率：";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(273, 20);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(152, 21);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "网站监控";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this._dgvOrderbook)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView _dgvOrderbook;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ 监控软件模板/监控软件模板/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using QR.Common.Http;
+using QR.Common.Tools;
+
+namespace Orderbook
+{
+    public partial class Form1 : Form
+    {
+        public static decimal min = 0;
+        public static int count = 0;
+
+
+        enum OrderSide
+        {
+            Buy = 1,
+            Sell = -1
+        }
+
+        enum OrderStatus
+        {
+            Working = 1,
+            Filled = 2,
+            Canceled = 3
+        }
+
+        private class OrderUpdate
+        {
+            public string Price { get; set; }
+            public string Quantity { get; set; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitDataTableObject();
+
+
+
+
+            this._dgvOrderbook.DataSource = this.sourceData;
+            //_incomingOrderThread = new Thread(InComingWorkerProcessor);
+            //_incomingOrderThread.Start();
+
+            ProgressBar.CheckForIllegalCrossThreadCalls = false;
+            tm.Interval = 1;
+            tm.Tick += new EventHandler(tm_Tick);
+            tm.Start();
+        }
+
+        /// <summary>
+        /// 初始化DataTable结构
+        /// </summary>
+        private void InitDataTableObject()
+        {
+
+            sourceData.Columns.Add("Price");
+            sourceData.Columns.Add("Quantity");
+
+        }
+        private OrderUpdate xx()
+        {
+            HttpWebPage openWeb = new HttpWebPage();
+            string activeUrl = "http://www.mysdic.com/join/invite.html";
+            string tempHtml = openWeb.DoGet(activeUrl);
+            string count1 = tempHtml.Abstract("fnt_24", "详细内容");
+            if (count1.Contains("招聘"))
+            {
+                List<string> ss = RegexHelper.GetMatchsList(@"\d{4}年\d{2}月\d{2}日", count1);
+                if (ss.Count > 0)
+                {
+                    DateTime start = this.dateTimePicker1.Value;
+                    DateTime end = new DateTime(
+                        int.Parse(ss[0].Abstract("", "年")),
+                        int.Parse(ss[0].Abstract("年", "月")),
+                        int.Parse(ss[0].Abstract("月", "日")));
+                    TimeSpan ts = end - start;
+                    if (ts.TotalSeconds > 0)
+                    {
+                        this.dateTimePicker1.Value = end;
+                        OrderUpdate record = new OrderUpdate();
+                        record.Price = ss[0];
+                        record.Quantity = count1.Abstract("blank\">​", "</a>");
+                        return record;
+                    }
+                }
+
+            }
+            return null;
+        }
+
+        private Thread _incomingOrderThread;
+        DataTable sourceData = new DataTable("Order");
+        //更新sourceData的线程
+        private Thread _updateUIThread;
+        private System.Windows.Forms.Timer tm = new System.Windows.Forms.Timer();
+        AutoResetEvent autoEvent = new AutoResetEvent(false);
+        Queue<DataRow> query = new Queue<DataRow>();
+
+        private void InComingWorkerProcessor()
+        {
+            while (true)
+            {
+                OrderUpdate update = xx();
+                if (update != null)
+                {
+                    OnOrderUpdate(update);
+                }
+                this.toolStripStatusLabel1.Text = "第" + count + "次访问网站";
+                count++;
+                Thread.Sleep((int)(this.numericUpDown1.Value * 1000 * 60));
+            }
+        }
+
+        void tm_Tick(object sender, EventArgs e)
+        {
+            HttpWebPage openWeb = new HttpWebPage();
+            //openWeb.DoGet("");
+            autoEvent.Set();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "暂停")
+            {
+                //暂停 grid update
+                tm.Stop();
+                if (this._incomingOrderThread.IsAlive)
+                {
+                    this._incomingOrderThread.Abort();
+                }
+                button1.Text = "开始";
+            }
+            else if (button1.Text == "开始")
+            {
+
+                //继续 grid update
+                tm.Start();
+                button1.Text = "暂停";
+                _incomingOrderThread = new Thread(InComingWorkerProcessor);
+                _incomingOrderThread.Start();
+            }
+        }
+
+        private void OnOrderUpdate(OrderUpdate update)
+        {
+            DataRow row = OrderObjToDataRow(update);
+            sourceData.Rows.Add(row);
+        }
+        private DataRow OrderObjToDataRow(OrderUpdate order)
+        {
+            DataRow row = sourceData.NewRow();
+
+            row["Price"] = order.Price;
+            row["Quantity"] = order.Quantity;
+
+            return row;
+        }
+
+        /// <summary>
+        /// 数据更新和处理
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="row"></param>
+        private void DataRowDataDeal()
+        {
+            while (true)
+            {
+                autoEvent.WaitOne();
+                if (query.Count > 0)
+                {
+                    bool containsFlag = false;
+                    DataRow row = query.Dequeue();
+                    sourceData.Rows.Add(row);
+                }
+            }
+        }
+
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (this._incomingOrderThread != null && this._incomingOrderThread.IsAlive)
+            {
+                this._incomingOrderThread.Abort();
+            }
+            if (this._updateUIThread != null && this._updateUIThread.IsAlive)
+            {
+                this._updateUIThread.Abort();
+            }
+        }
+
+    }
+}
